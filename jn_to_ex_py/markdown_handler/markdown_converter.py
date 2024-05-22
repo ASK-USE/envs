3f818d337beaa9b55d5ed1e0a4c9eba296e84456
ASK <Knueppelalex@web.de>
2024-05-22 15:38:17 +0200

    created a new function in markdownconverter, maybe problem solved?

--- jn_to_ex_py/markdown_handler/markdown_converter.py
+++ jn_to_ex_py/markdown_handler/markdown_converter.py
@@ -1,5 +1,7 @@
-#markdown_converter.py
+# markdown_converter.py
 import os
+import os.path
+import nbformat
 from ..media_handler.media_structure_generator import MediaHandler
 from ..media_handler.image_handler import find_image_references
 
@@ -13,6 +15,13 @@ def main():
     convert_markdown_to_python(notebook_file)
     media_references = extract_media_references(notebook_file)
     MediaHandler.process_media_references(media_references)
+    
+def extract_and_display_images(notebook_file):
+    with open(notebook_file, 'r') as f:
+        notebook = nbformat.read(f, as_version=4)
+    image_references = find_image_references(notebook.cells)
+    # Hier können Sie den Code zum Anzeigen der Bilder implementieren
+    print(f"Extrahierte Bildverweise: {image_references}")
 
 def convert_markdown_to_python(notebook_file):
     # Funktion zur Konvertierung des Markdown-Notebooks in eine Python-Datei
