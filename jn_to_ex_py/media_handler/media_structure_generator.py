2fa24c629e834a9680a9891dc775136a43f947d4
ASK <Knueppelalex@web.de>
2024-05-22 14:42:33 +0200

    deleted some unnessesary importdots

--- jn_to_ex_py/media_handler/media_structure_generator.py
+++ jn_to_ex_py/media_handler/media_structure_generator.py
@@ -1,7 +1,7 @@
 # media_structure_generator.py
-from .url_validator import is_valid_url
-from .image_handler import find_image_references
-from .video_handler import find_video_references
+from url_validator import is_valid_url
+from image_handler import find_image_references
+from video_handler import find_video_references
 
 class MediaItem:
     def __init__(self, item_type, reference):
