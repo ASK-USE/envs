e60326ec5bbf33a255e3f045b39ebcc1ca2f3c0b
ASK <Knueppelalex@web.de>
2024-05-22 14:07:38 +0200

    changed the iput-folders hardcoded path to a realtive path

--- jn_to_ex_py/markdown_handler/markdown_converter.py
+++ jn_to_ex_py/markdown_handler/markdown_converter.py
@@ -1,7 +1,18 @@
 #markdown_converter.py
-from media_handler.media_structure_generator import MediaHandler
-# import nbformat
+import os
+from ..media_handler.media_structure_generator import MediaHandler
 from ..media_handler.image_handler import find_image_references
+
+def main():
+    # Relativer Pfad zum Eingabeordner
+    input_dir = os.path.join(os.path.dirname(__file__), '..', 'input')
+    
+    # Relativer Pfad zur Notebook-Datei
+    notebook_file = os.path.join(input_dir, 'notebooksample.ipynb')
+    
+    convert_markdown_to_python(notebook_file)
+    media_references = extract_media_references(notebook_file)
+    MediaHandler.process_media_references(media_references)
 
 def convert_markdown_to_python(notebook_file):
     # Funktion zur Konvertierung des Markdown-Notebooks in eine Python-Datei
@@ -10,18 +21,6 @@ def convert_markdown_to_python(notebook_file):
 def extract_media_references(notebook_file):
     # Funktion zur Extraktion von Medienverweisen aus dem Markdown-Notebook
     pass
-
-def main():
-    notebook_file = "/home/ask/Dokumente/repositorys/envs/jn_to_ex_py/input/notebooksample.ipynb"
-    
-    # Konvertiere Markdown-Datei in Python-Datei
-    convert_markdown_to_python(notebook_file)
-    
-    # Extrahiere Medienverweise aus dem Markdown-Notebook
-    media_references = extract_media_references(notebook_file)
-    
-    # Verarbeite die extrahierten Medienverweise mit dem MediaHandler
-    MediaHandler.process_media_references(media_references)
 
 if __name__ == "__main__":
     main()
