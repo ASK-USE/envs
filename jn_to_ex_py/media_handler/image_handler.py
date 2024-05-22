8830ccc0a68081ba8f77510d8fff7e3d2da39582
ASK <Knueppelalex@web.de>
2024-05-22 12:56:14 +0200

    reworked malfunctioning code in image handler and markdowwnconverter

--- jn_to_ex_py/media_handler/image_handler.py
+++ jn_to_ex_py/media_handler/image_handler.py
@@ -1,8 +1,9 @@
+# image_handler.py
 import re
 
 def find_image_references(notebook_content):
     image_references = []
-    pattern = r"!\[.*\]\((.*?)\)"  # Regex-Pattern zum Extrahieren des Bild-Pfads aus Markdown-Links
+    pattern = r"!\[.*\]\((.*?)\)"
 
     for cell in notebook_content:
         if cell["cell_type"] == "markdown":
@@ -10,8 +11,3 @@ def find_image_references(notebook_content):
             image_references.extend(matches)
 
     return image_references
-
-# Beispielaufruf
-notebook_content = [...]  # Hier kommt der Inhalt des Notebooks als Liste von Zellenobjekten
-image_references = find_image_references(notebook_content)
-print(image_references)
