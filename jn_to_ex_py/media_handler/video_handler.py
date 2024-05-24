3ba09a33607be38d5c0bc14f72925a4c172ffd95
ASK <Knueppelalex@web.de>
2024-05-24 12:45:30 +0200

    cleared and sorted the imports and the difinitions etc. in urlvalidator, videohandler and main

--- jn_to_ex_py/media_handler/video_handler.py
+++ jn_to_ex_py/media_handler/video_handler.py
@@ -1,23 +1,17 @@
-#video_handler.py
+# video_handler.py
+
 import re
+from media_handler.url_validator import notebook_content, valid_extensions
 
 def find_video_references(notebook_content, valid_extensions):
     video_references = []
-    pattern = rf"\[.*\]\((.*?)\.({'|'.join(valid_extensions)})\)"
-
+    pattern = rf"\[.*\]\((.*?)\\.({'|'.join(valid_extensions)})\)"
     for cell in notebook_content:
         if cell["cell_type"] == "markdown":
             matches = re.findall(pattern, cell["source"])
             video_references.extend(matches)
-
     return video_references
 
 # Beispielaufruf
-notebook_content = [
-    {"cell_type": "markdown", "source": "# Heading\n[Some video](video.mp4)"},
-    {"cell_type": "code", "source": "print('Hello')"},
-    {"cell_type": "markdown", "source": "Another [video](video.avi)"}
-]  # Hier kommt der Inhalt des Notebooks als Liste von Zellenobjekten
-valid_extensions = ['mp4', 'avi', 'mov', 'mkv', 'wmv']  # Liste der gültigen Dateierweiterungen für Videos
 video_references = find_video_references(notebook_content, valid_extensions)
 print(video_references)
